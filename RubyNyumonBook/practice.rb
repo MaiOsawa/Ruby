07a29092eaf52726a7d24b765d227568731168d2
Mary Major <mary.major@example.comMai>
2020-07-21 14:14:01 +0000

    second commit

--- RubyNyumonBook/practice.rb
+++ RubyNyumonBook/practice.rb
@@ -41,3 +41,45 @@ end
 { height: 175, weight: 70 }.each do |element|
   p element
 end
+
+#メソッド
+
+
+ret = 1.to_s(2)
+p ret
+
+ret = "test".size
+p ret
+
+p "test"
+sleep 1
+
+p "1,2,3".split(",")
+p [1, 2, 3].join(".")
+
+p Time.now
+
+def calc_triangle_area(base, height)
+  base * height / 2
+end
+
+p calc_triangle_area(10, 5)
+
+
+def calc_triangle_area(base, height)
+  base * height / 2
+end
+
+mybase = 12
+myheight = 3
+
+p calc_triangle_area(mybase, myheight)
+
+def calc_tax(price, tax_rate: 0.08)
+  tax = (price * tax_rate).to_i
+  tax_included = price + tax
+  { tax: tax, tax_included: tax_included }
+end
+
+p calc_tax(100, tax_rate: 0.1)
+p calc_tax(100)
